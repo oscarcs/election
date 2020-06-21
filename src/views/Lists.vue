3e6dacf3d3f33a3d911d50574fd4fab7c8ac126a
Oscar Sims <oscar@oscarsims.co.nz>
2020-06-21 16:19:05 +1200

    Add party lists.

--- src/views/Lists.vue
+++ src/views/Lists.vue
@@ -4,9 +4,25 @@
             <article class="tile is-child notification is-slate">
                 <p class="is-size-5">Party Lists</p>
                 <p>The deadline for the political parties to submit their nominations for the party lists is 21 August.</p>
-                <select>
-                    <option></option>
-                </select>
+                <b-dropdown class="mt-1 mb-1">
+                    <button class="button" :class="[`is-${getPartyCSSName(selectedParty)}`]" slot="trigger" slot-scope="{ active }">
+                        <span class="mr-1">{{getPartyName(selectedParty)}}</span>
+                        <ion-icon
+                            :name="active ? 'chevron-up-outline' : 'chevron-down-outline'"
+                        ></ion-icon>
+                    </button>
+                    <b-dropdown-item v-for="party of Object.keys(lists)" :key="party" @click="changeSelectedParty(party)">
+                        {{getPartyName(party)}} ({{lists[party].length}})
+                    </b-dropdown-item>
+                </b-dropdown>
+                <div>
+                    <!-- <p class="is-size-5">Party List for {{getPartyName(selectedParty)}}</p> -->
+                    <div class="mt-1">
+                        <div class="notification is-charcoal partylist" v-for="(member, spot) of lists[selectedParty]" :key="member">
+                            {{spot + 1}} — {{member}}
+                        </div>
+                    </div>
+                </div>
             </article>
         </div>
     </div>
@@ -15,6 +31,7 @@
 <script lang="ts">
 import Vue from 'vue';
 import Config from '@/config';
+import { Util } from '@/util';
 
 export default Vue.extend({
     name: "Lists",
@@ -22,14 +39,40 @@ export default Vue.extend({
     computed: {
 
     },
+    data: function() {
+        return {
+            lists: {},
+            selectedParty: 'lab'
+        };
+    },
     mounted () {
-        fetch(`${Config.baseUrl}/electorates`)
+        fetch(`${Config.baseUrl}/lists`)
             .then(response => {
                 return response.json();
             })
             .then(data => {
-                console.log(data);
+                this.lists = data;
             });
+    },
+    methods: {
+        getPartyName: function(abbr: string): string {
+            return Util.getPartyName(abbr);
+        },
+
+        getPartyCSSName: function(abbr: string): string {
+            return Util.getPartyCSSName(abbr);
+        },
+
+        changeSelectedParty: function(party: string) {
+            this.selectedParty = party;
+        }
     }
 });
 </script>
+
+<style lang="scss">
+.partylist {
+    display: inline-block;
+    margin-right: 1.5rem;
+}
+</style>
